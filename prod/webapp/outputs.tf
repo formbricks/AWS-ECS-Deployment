e5d3e234d612852ddd184c3d2cb9299547c01993
Sudhanshu Pandey <sp6370@nyu.edu>
2024-01-29 14:40:36 +0000

    Update application_url to use HTTPS

--- prod/webapp/outputs.tf
+++ prod/webapp/outputs.tf
@@ -1,4 +1,4 @@
 output "application_url" {
-  value       = "http://${module.alb.dns_name}"
+  value       = "https://${module.alb.dns_name}"
   description = "Copy this value in your browser in order to access the deployed app"
 }
