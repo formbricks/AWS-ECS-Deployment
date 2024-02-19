4b15785172d85c857d1946290a5ce380f0a76681
Sudhanshu Pandey <sp6370@nyu.edu>
2024-02-19 17:13:29 -0500

    chore: update naming convention for resources

--- terraform/webapp-service-lb/main.tf
+++ terraform/webapp-service-lb/main.tf
@@ -20,16 +20,16 @@ terraform {
 */
 
 locals {
-    name           = "prod-webapp"
-    container_port = 3000
-    container_name = "prod-webapp-container"
-    tags = {
-        Environment = "prod"
-    }
+  name           = "webapp"
+  container_port = 3000
+  container_name = "webapp-container-formbricks"
+  tags = {
+    Environment = "prod"
+  }
 }
 
 provider "aws" {
-    region = var.region
+  region = var.region
 }
 
 ################################################################################
@@ -37,38 +37,38 @@ provider "aws" {
 ################################################################################
 
 data "aws_vpc" "vpc" {
-    filter {
-        name   = "tag:Name"
-        values = ["prod-core-infra-vpc"]
-    }
+  filter {
+    name   = "tag:Name"
+    values = ["core-infra-formbricks-vpc"]
+  }
 }
 
 data "aws_subnets" "public" {
-    filter {
-        name   = "tag:Name"
-        values = ["prod-core-infra-vpc-public-*"]
-    }
+  filter {
+    name   = "tag:Name"
+    values = ["core-infra-formbricks-vpc-public-*"]
+  }
 }
 
 data "aws_subnets" "private" {
-    filter {
-        name   = "tag:Name"
-        values = ["prod-core-infra-vpc-private-*"]
-    }
+  filter {
+    name   = "tag:Name"
+    values = ["core-infra-formbricks-vpc-private-*"]
+  }
 }
 
 data "aws_subnet" "private_cidr" {
-    for_each = toset(data.aws_subnets.private.ids)
-    id       = each.value
+  for_each = toset(data.aws_subnets.private.ids)
+  id       = each.value
 }
 
 data "aws_ecs_cluster" "core_infra" {
-    cluster_name = "prod-core-infra-ecs-cluster"
+  cluster_name = "core-infra-formbricks-cluster"
 }
 
 data "aws_service_discovery_dns_namespace" "this" {
-    name = "prod-core-infra-service-discovery-private-dns-namespace"
-    type = "DNS_PRIVATE"
+  name = "core-infra-formbricks-service-discovery-private-dns-namespace"
+  type = "DNS_PRIVATE"
 }
 
 ################################################################################
@@ -76,169 +76,168 @@ data "aws_service_discovery_dns_namespace" "this" {
 ################################################################################
 
 module "ecs_service" {
-    source  = "terraform-aws-modules/ecs/aws//modules/service"
-    version = "~> 5.6"
+  source  = "terraform-aws-modules/ecs/aws//modules/service"
+  version = "~> 5.6"
 
-    name          = "prod-webapp-ecs-service"
-    cluster_arn   = data.aws_ecs_cluster.core_infra.arn
+  name        = "formbricks-webapp-service"
+  cluster_arn = data.aws_ecs_cluster.core_infra.arn
 
-    enable_execute_command = false
+  enable_execute_command = false
 
-    desired_count = 3
+  desired_count = 3
 
-    autoscaling_min_capacity = 3
-    autoscaling_max_capacity = 6
-    
+  autoscaling_min_capacity = 3
+  autoscaling_max_capacity = 6
 
-    # Task Definition IAM Roles
-    create_task_exec_iam_role = true
-    task_exec_iam_role_name   = "ecsTaskExecRole-prod-webapp-tasks"
-    create_task_exec_policy   = true
-    task_exec_secret_arns     = values(var.secrets_manager_data)[*]
-    task_exec_ssm_param_arns  = []
 
-    container_definitions = {
-        (local.container_name) = {
-            image                    = var.TF_VAR_container_image
-            cpu                      = "1024"
-            memory                   = "2048"
-            readonly_root_filesystem = false
-            port_mappings = [
-                {
-                    protocol      = "tcp"
-                    containerPort = local.container_port
-                }
-            ]
-            secrets = [
-                for key, value in var.secrets_manager_data :
-                {
-                    name      = key
-                    valueFrom = "${value}:${key}::"
-                }
-            ]
-            environment = []
+  # Task Definition IAM Roles
+  create_task_exec_iam_role = true
+  task_exec_iam_role_name   = "ecsTaskExecRole-prod-webapp-tasks"
+  create_task_exec_policy   = true
+  task_exec_secret_arns     = values(var.secrets_manager_data)[*]
+  task_exec_ssm_param_arns  = []
+
+  container_definitions = {
+    (local.container_name) = {
+      image                    = var.TF_VAR_container_image
+      cpu                      = "1024"
+      memory                   = "2048"
+      readonly_root_filesystem = false
+      port_mappings = [
+        {
+          protocol      = "tcp"
+          containerPort = local.container_port
         }
-    }
-
-    service_registries = {
-        registry_arn = aws_service_discovery_service.this.arn
-    }
-
-    load_balancer = {
-        service = {
-            target_group_arn = module.alb.target_groups["ecs-task"].arn
-            container_name   = local.container_name
-            container_port   = local.container_port
+      ]
+      secrets = [
+        for key, value in var.secrets_manager_data :
+        {
+          name      = key
+          valueFrom = "${value}:${key}::"
         }
+      ]
+      environment = []
     }
+  }
 
-    subnet_ids = data.aws_subnets.private.ids
+  service_registries = {
+    registry_arn = aws_service_discovery_service.this.arn
+  }
 
-    security_group_rules = {
-        ingress_alb_service = {
-            type                     = "ingress"
-            from_port                = local.container_port
-            to_port                  = local.container_port
-            protocol                 = "tcp"
-            description              = "Service port"
-            source_security_group_id = module.alb.security_group_id
-        }
-        egress_all = {
-            type        = "egress"
-            from_port   = 0
-            to_port     = 0
-            protocol    = "-1"
-            cidr_blocks = ["0.0.0.0/0"]
-        }
+  load_balancer = {
+    service = {
+      target_group_arn = module.alb.target_groups["ecs-task"].arn
+      container_name   = local.container_name
+      container_port   = local.container_port
     }
+  }
 
-    tags = local.tags
+  subnet_ids = data.aws_subnets.private.ids
+
+  security_group_rules = {
+    ingress_alb_service = {
+      type                     = "ingress"
+      from_port                = local.container_port
+      to_port                  = local.container_port
+      protocol                 = "tcp"
+      description              = "Service port"
+      source_security_group_id = module.alb.security_group_id
+    }
+    egress_all = {
+      type        = "egress"
+      from_port   = 0
+      to_port     = 0
+      protocol    = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+
+  tags = local.tags
 }
 
 resource "aws_service_discovery_service" "this" {
-    name = "${local.name}-service-discovery"
+  name = "${local.name}-service-discovery"
 
-    dns_config {
-        namespace_id = data.aws_service_discovery_dns_namespace.this.id
+  dns_config {
+    namespace_id = data.aws_service_discovery_dns_namespace.this.id
 
-        dns_records {
-            ttl  = 10
-            type = "A"
-        }
-
-        routing_policy = "MULTIVALUE"
+    dns_records {
+      ttl  = 10
+      type = "A"
     }
 
-    health_check_custom_config {
-        failure_threshold = 1
-    }
+    routing_policy = "MULTIVALUE"
+  }
+
+  health_check_custom_config {
+    failure_threshold = 1
+  }
 }
 
 module "alb" {
-    source  = "terraform-aws-modules/alb/aws"
-    version = "~> 9.0"
+  source  = "terraform-aws-modules/alb/aws"
+  version = "~> 9.0"
 
-    name = "${local.name}-alb"
+  name = "${local.name}-alb"
 
-    # For Prod only
-    enable_deletion_protection = true
+  # For Prod only
+  enable_deletion_protection = true
 
-    vpc_id  = data.aws_vpc.vpc.id
-    subnets = data.aws_subnets.public.ids
+  vpc_id  = data.aws_vpc.vpc.id
+  subnets = data.aws_subnets.public.ids
 
-    security_group_ingress_rules = {
-      all_https = {
+  security_group_ingress_rules = {
+    all_https = {
       from_port   = 443
       to_port     = 443
       ip_protocol = "tcp"
       description = "HTTPS web traffic"
       cidr_ipv4   = "0.0.0.0/0"
     }
+  }
+
+  security_group_egress_rules = {
+    for subnet in data.aws_subnet.private_cidr :
+    (subnet.availability_zone) => {
+      ip_protocol = "-1"
+      cidr_ipv4   = subnet.cidr_block
     }
+  }
 
-    security_group_egress_rules = {
-        for subnet in data.aws_subnet.private_cidr :
-        (subnet.availability_zone) => {
-            ip_protocol = "-1"
-            cidr_ipv4   = subnet.cidr_block
-        }
+  listeners = {
+    https = {
+      port            = 443
+      protocol        = "HTTPS"
+      certificate_arn = var.formbricks_ssl_certificate_arn
+      forward = {
+        target_group_key = "ecs-task"
+      }
     }
+  }
 
-    listeners = {
-        https = {
-                port            = 443
-                protocol        = "HTTPS"
-                certificate_arn = var.formbricks_ssl_certificate_arn
+  target_groups = {
+    ecs-task = {
+      backend_protocol = "HTTP"
+      backend_port     = local.container_port
+      target_type      = "ip"
 
-            forward = {
-                target_group_key = "ecs-task"
-            }
-        }
+      health_check = {
+        enabled             = true
+        healthy_threshold   = 5
+        interval            = 30
+        matcher             = "200-299"
+        path                = "/health"
+        port                = "traffic-port"
+        protocol            = "HTTP"
+        timeout             = 5
+        unhealthy_threshold = 2
+      }
+
+      # There's nothing to attach here in this definition. Instead,
+      # ECS will attach the IPs of the tasks to this target group
+      create_attachment = false
     }
+  }
 
-    target_groups = {
-        ecs-task = {
-            backend_protocol = "HTTP"
-            backend_port     = local.container_port
-            target_type      = "ip"
-
-            health_check = {
-                enabled             = true
-                healthy_threshold   = 5
-                interval            = 30
-                matcher             = "200-299"
-                path                = "/health"
-                port                = "traffic-port"
-                protocol            = "HTTP"
-                timeout             = 5
-                unhealthy_threshold = 2
-            }
-
-            # There's nothing to attach here in this definition. Instead,
-            # ECS will attach the IPs of the tasks to this target group
-            create_attachment = false
-        }
-    }
-
-    tags = local.tags
+  tags = local.tags
 }
