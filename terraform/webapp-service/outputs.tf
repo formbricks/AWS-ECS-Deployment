1073632b1b91f1333108052ae9b371f55460259d
Sudhanshu Pandey <sp6370@nyu.edu>
2024-02-28 03:29:11 +0000

    Update application URL to use HTTP instead of HTTPS

--- terraform/webapp-service/outputs.tf
+++ terraform/webapp-service/outputs.tf
@@ -1,4 +1,4 @@
 output "application_url" {
-  value       = "https://${module.alb.dns_name}"
+  value       = "http://${module.alb.dns_name}"
   description = "Copy this value in your browser in order to access the deployed app"
 }
